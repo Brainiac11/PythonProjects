f99b7e58625a4a0d9ac7f16e84536fd48c979798
Saathvik Kamma <saathvikkamma@gmail.com>
2022-07-15 10:56:06 -0500

    Added Scientific Calculator Functionality

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,61 @@
-print("hello World")
-input('calcultor time')
+
+import math
+import sys
+import os
+print("Welcome to the calculator!")
+print("These are the available operations:")
+print("1. Addition(+)")
+print("2. Subtraction(-)")
+print("3. Multiplication(*)")
+print("4. Division(/)")
+print("5. Exponentiation(^)")
+print("6. Square Root(sqrt)")
+print("7. Logarithm(log)")
+print("8. Sinus(sin) [radians]")
+print("9. Cosinus(cos) [radians]")
+print("10. Tangens(tan) [radians]")
+print("^-1 is the inverse of the function or number")
+print("^2 is the square of the number")
+print("^3 is the cube of the number")
+print("^4 is the fourth power of the number")
+number1 = input("Type out your first number: ")
+try:
+    number1 = float(number1)
+except ValueError:
+    print("You have to type a number!")
+    sys.exit()
+operator1 = input("Type out your operator: ")
+try:
+   if operator1 == '+' or '-' or '*' or '/' or '^' or 'sqrt' or 'log' or 'sin' or 'cos' or 'tan' :
+         pass
+except ValueError:
+    print("You have to type a valid operator!")
+    sys.exit()
+number2 = input("Type out your second number: ")
+
+try:
+    number2 = float(number2)
+except ValueError:
+    print("You have to type a number!")
+    sys.exit()
+if operator1 == '+':
+    print(number1 + number2)
+elif operator1 == '-':
+    print(number1 - number2)
+elif operator1 == '*':
+    print(number1 * number2)
+elif operator1 == '/':
+    print(number1 / number2)
+elif operator1 == '^':
+    print(number1 ** number2)
+elif operator1 == 'sqrt':
+    print(math.sqrt(number1))
+elif operator1 == 'log':
+    print(math.log(number1))
+elif operator1 == 'sin':
+    print(math.sin(number1))
+elif operator1 == 'cos':
+    print(math.cos(number1))
+elif operator1 == 'tan':
+    print(math.tan(number1))
+
