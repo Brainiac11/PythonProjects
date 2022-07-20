2c228e5b7f2a22c37dfc3fa8bdb845b3853cc33b
Saathvik Kamma <saathvikkamma@gmail.com>
2022-07-19 20:12:33 -0500

    Working on fixing the issue

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,9 @@
-
+import cmath
 import math
 import sys
 import os
 from xml.dom.pulldom import IGNORABLE_WHITESPACE
+
 f  =open ("Log.txt", "w")
 
 print("Welcome to the calculator!")
@@ -14,9 +15,9 @@ print("4. Division(/)")
 print("5. Exponentiation(^)")
 print("6. Square Root(sqrt)")
 print("7. Logarithm(log)")
-print("8. Sinus(sin) [radians]")
-print("9. Cosinus(cos) [radians]")
-print("10. Tangens(tan) [radians]")
+print("8. Sinus(sin) [degrees]")
+print("9. Cosinus(cos) [degrees]")
+print("10. Tangens(tan) [degrees]")
 print("^-1 is the inverse of the function or number")
 print("^2 is the square of the number")
 print("^3 is the cube of the number")
@@ -28,19 +29,27 @@ except ValueError:
     print("You have to type a number!")
     sys.exit()
 operator1 = input("Type out your operator: ")
-try:
-   if operator1 == '+' or '-' or '*' or '/' or '^' or 'sqrt' or 'log' or 'sin' or 'cos' or 'tan' or IGNORABLE_WHITESPACE('sin^-1') or IGNORABLE_WHITESPACE('cos^-1') or IGNORABLE_WHITESPACE('tan^-1'):
-         pass
-except ValueError:
-    print("You have to type a valid operator!")
-    sys.exit()
-number2 = input("Type out your second number: ")
+    #operator1 == '+' or '-' or '*' or '/' or '^' or 'sqrt' or 'log' or 'sin' or 'cos' or 'tan' or ('tan^-1') or ('cos^-1') or ('sin^-1')
+ch = ['+' , '-' , '*' , '/' , '^' , 'sqrt' , 'log' , 'sin' , 'cos' , 'tan' , 'tan^-1' ,'cos^-1' ,'sin^-1']
+ch_trig = ['sin' , 'cos' , 'tan' , 'tan^-1' ,'cos^-1' ,'sin^-1' 'sqrt' , 'log' ,'^']
+if operator1 in ch:
+    pass
 
-try:
-    number2 = float(number2)
-except ValueError:
-    print("You have to type a number!")
+else:
+    print("You have to type an operator!")
     sys.exit()
+if operator1 in ch_trig:
+    pass
+else:
+    number2 = input("Type out your second number: ")
+    try:
+        number2 = float(number2)
+    except ValueError:
+        print("You have to type a number!")
+        sys.exit()
+
+    
+
 if operator1 == '+':
     answer = number1 + number2
     print("The answer is: " + str(answer))
@@ -67,47 +76,54 @@ elif operator1 == '^':
     with open('Log.txt', 'w') as f:
         f.write(str(number1) + str(operator1) + str(number2) + '=' + str(answer))
 elif operator1 == 'sqrt':
-    answer = math.sqrt(number1)
+    answer = cmath.sqrt(number1)
     print(answer)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'log':
-    answer = math.log(number1)
+    answer = cmath.log(number1)
     print(answer)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'sin':
-    answer = math.sin(number1)
-    print(answer)
+    answer = cmath.sin(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'cos':
-    answer = math.cos(number1)
-    print(answer)
+    answer = cmath.cos(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'tan':
-    answer = math.tan(number1)
-    print(answer)
+    answer = cmath.tan(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'sin^-1':
-   answer= math.asin(number1)
-   print(answer)
-   with open('Log.txt', 'w') as f:
+    answer= cmath.asin(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
+    with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'cos^-1':
-    answer = math.acos(number1)
-    print(answer)
+    answer = cmath.acos(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
 elif operator1 == 'tan^-1':
-    answer  = math.atan(number1)
-    print(answer)
+    answer  = cmath.atan(number1)
+    answerln =  cmath.degrees(answer)
+    print(answerln)
     with open('Log.txt', 'w') as f:
         f.write(str(operator1) + '(' + str(number1) + ')' '=' + str(answer))
         
 f.close()
 
     
+
 
